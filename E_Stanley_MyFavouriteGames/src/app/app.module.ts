9c89be59647d71eb00c029bcb77c3e23b2085994
Evan Stanley <A1ZHU257@gmail.com>
2022-02-22 04:36:53 -0500

    Assignment4

--- E_Stanley_MyFavouriteGames/src/app/app.module.ts
+++ E_Stanley_MyFavouriteGames/src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { ContentCardComponent } from './content-card/content-card.component';
 import { ContentListComponent } from './content-list/content-list.component';
 import { ContentListPipe } from './content-list.pipe';
+import { HoverAffectDirective } from './hover-affect.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     ContentCardComponent,
     ContentListComponent,
-    ContentListPipe
+    ContentListPipe,
+    HoverAffectDirective
   ],
   imports: [
     BrowserModule
